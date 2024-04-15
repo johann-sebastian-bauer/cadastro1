c95cd6224e202bb374081f39858c429c4728c017
johann_bauer <112898573+johann-sebastian-bauer@users.noreply.github.com>
2024-04-15 11:30:59 -0300

    ta feito

---
+++ tela de login/script.js
@@ -0,0 +1,17 @@
+function cadastro(){
+
+var nome = document.getElementById("nome").value
+var email = document.getElementById("email").value
+var senha = document.getElementById("senha").value
+var idade = document.getElementById("idade").value
+var cidade = document.getElementById("cidade").value
+
+var usuario = {
+    nome: nome,
+    email:email,
+    senha:senha,
+    idade:idade,
+    cidade:cidade
+}
+
+console.log(usuario);}
